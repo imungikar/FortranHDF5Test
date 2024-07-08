1d07193093c6be74cd14e9bedd96f5b0d1189495
imungikar <imungikar@gmail.com>
2024-07-08 16:05:56 -0400

    let's see

---
+++ hdf5test.f90
@@ -0,0 +1,41 @@
+program hdf5test.f90
+    use hdf5
+    implicit none
+
+    ! Variables
+    integer(hid_t) :: file_id, dataset_id, dataspace_id  ! Handles for file, dataset, and dataspace
+    integer(hdferr) :: error                             ! Error handling
+    integer, parameter :: rank = 2                       ! Rank of the dataset (2D array)
+    integer, dimension(2) :: dims = [4, 6]               ! Dimensions of the dataset
+    real(8), dimension(4, 6) :: data                     ! Data to write to the dataset
+    integer :: i, j
+
+    ! Initialize the data array
+    do i = 1, 4
+        do j = 1, 6
+            data(i, j) = real(i * j)
+        end do
+    end do
+
+    ! Create a new HDF5 file using default properties
+    file_id = h5fcreate('example.h5', H5F_ACC_TRUNC_F, H5P_DEFAULT_F, H5P_DEFAULT_F, error)
+
+    ! Create the data space for the dataset
+    dataspace_id = h5screate_simple(rank, dims, dims, error)
+
+    ! Create the dataset
+    dataset_id = h5dcreate_f(file_id, 'dset', H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT_F, H5P_DEFAULT_F, H5P_DEFAULT_F, error)
+
+    ! Write the data to the dataset
+    call h5dwrite_f(dataset_id, H5T_NATIVE_DOUBLE, data, dims, error)
+
+    ! Close the dataset and dataspace
+    call h5dclose_f(dataset_id, error)
+    call h5sclose_f(dataspace_id, error)
+
+    ! Close the file
+    call h5fclose_f(file_id, error)
+
+    print *, 'HDF5 file created and dataset written successfully.'
+
+end program hdf5test.f90
--- instructions.md
+++ instructions.md
@@ -5,4 +5,19 @@ Building the Repository on Stellar Cluster
 
 Committing Changes Through VS Code
 - go to source control (which looks like branches)
+- Click the upper left check mark, but make sure to hit on commit and push
 - enter your commit message on the first thing, and then click the check mark in the upper-right side of the screen
+
+
+
+Project Goals
+- .bin and .dat --> just clean up
+- checkpoints and snapshots --> fix for efficiency
+- get rid of MPI barriers, optimize the parallel processing
+- (worth learning all the Git features from the command line later)
+- On resume talk about how this is software is used by various companies, optimizes code by x%
+
+
+Links
+- https://docs.hdfgroup.org/archive/support/HDF5/doc/RM/RM_H5Front.html
+https://docs.hdfgroup.org/archive/support/HDF5/doc/Intro/IntroExamples.html#CreateExample
